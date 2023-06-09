b1d2c16d318725cadb2555cc9989a0415210b357
sudhir17 <65382006+sudhir17@users.noreply.github.com>
2023-06-09 19:44:28 +0530

    Create MajorityElement.java

---
+++ MajorityElement.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.* ;
+
+public class Solution {
+	public static int findMajority(int[] arr, int n) {
+		// Write your code here.
+	HashMap<Integer,Integer> map=new HashMap<>();
+        int count=(int)Math.floor(n/2);
+        for(int i=0;i<arr.length;i++)
+        {
+            if(map.containsKey(arr[i]))
+            {
+                map.put(arr[i],map.get(arr[i])+1);
+                
+            }
+            else 
+                map.put(arr[i],1);
+        }
+         for(int i=0;i<arr.length;i++)
+        {
+            if(map.get(arr[i])>count)
+                return arr[i];
+        }
+        
+         return -1;     
+		
+	}
+}

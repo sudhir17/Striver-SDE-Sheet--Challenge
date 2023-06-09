c510abea7886f6f524073ce267d47a337bf1bf10
sudhir17 <65382006+sudhir17@users.noreply.github.com>
2023-06-09 10:21:46 +0530

    Create FindDuplicateInArray.java

---
+++ FindDuplicateInArray.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.* ;
+
+import java.util.ArrayList;
+
+public class Solution{
+    public static int findDuplicate(ArrayList<Integer> arr, int n){
+        // Write your code here.
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<n;i++)
+        {
+            if(set.contains(arr.get(i)))
+                return arr.get(i);
+            set.add(arr.get(i));
+        }
+        return -1;
+    }
+}

e231a559e23c4157f53e44b0126b341e1a3afc7c
sudhir17 <65382006+sudhir17@users.noreply.github.com>
2023-07-29 18:57:20 +0530

    Create MedianOfTwoSortedArrays.java

---
+++ MedianOfTwoSortedArrays.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public static double median(int[] a, int[] b) {
+    	// Write your code here.
+        int n=a.length+b.length;
+        int res[]=new int[n];
+        int i=0,j=0,k=0;
+        while(i<a.length && j<b.length)
+        {
+            if(a[i]<=b[j])
+                res[k++]=a[i++];
+            else 
+                res[k++]=b[j++];
+        }
+        while(i<a.length)
+            res[k++]=a[i++];
+        while(j<b.length)
+            res[k++]=b[j++];
+        double ans=0;
+        if(res.length%2==0)
+        {
+            int len=res.length/2;
+            ans=(res[len]+res[len-1])/2.0;
+        }
+        else 
+            ans=res[res.length/2];
+        return ans;
+    }
+}

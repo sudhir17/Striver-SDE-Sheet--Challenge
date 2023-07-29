835767824f7d00281b66e304b0f17f7c2051df6f
sudhir17 <65382006+sudhir17@users.noreply.github.com>
2023-07-29 19:08:27 +0530

    Create ValidParentheses.java

---
+++ ValidParentheses.java
@@ -0,0 +1,25 @@
+public class Solution{
+    static class Queue {
+        // Define the data members(if any) here.
+
+        Queue() {
+            // Initialize your data structure here.
+        }
+
+        void enQueue(int val) {
+            // Implement the enqueue() function.
+        }
+
+        int deQueue() {
+            // Implement the dequeue() function.
+        }
+
+        int peek() {
+            // Implement the peek() function here.
+        }
+
+        boolean isEmpty() {
+            // Implement the isEmpty() function here.
+        }
+    }
+}

b57e38f87ffe40e8ff6768dbd7ed37da98236c5b
sudhir17 <65382006+sudhir17@users.noreply.github.com>
2023-07-30 10:13:48 +0530

    Create ConvertAGivenBinaryTreeToDoublyLInkedList.java

---
+++ ConvertAGivenBinaryTreeToDoublyLInkedList.java
@@ -0,0 +1,28 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+
+	public static BinaryTreeNode<Integer> BTtoDLL(BinaryTreeNode<Integer> root) {
+        List<BinaryTreeNode<Integer>> prev = new LinkedList<BinaryTreeNode<Integer>>();
+        prev.add(null);
+        BTtoDLL(root, prev);
+        if (prev.get(0) != null) {
+            prev.get(0).left = null;
+        }
+        return prev.get(0);
+	}
+
+    private static void BTtoDLL(BinaryTreeNode<Integer> root, List<BinaryTreeNode<Integer>> prev) {
+        if (root != null) {
+            BTtoDLL(root.right, prev);
+            root.right = prev.get(0);
+            if (prev.get(0) != null) {
+                root.right.left = root;
+            }
+            prev.set(0, root);
+            BTtoDLL(root.left, prev);
+        }
+    }
+
+}

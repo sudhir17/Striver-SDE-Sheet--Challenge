3dd5d56b61c909cc2b14ea39d482d88136d5fca1
sudhir17 <65382006+sudhir17@users.noreply.github.com>
2023-07-30 10:27:20 +0530

    Update MaximumSumIncreasingSubsequence.java

--- MaximumSumIncreasingSubsequence.java
+++ MaximumSumIncreasingSubsequence.java
@@ -1,43 +1,33 @@
-import java.util.*;
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+
 public class Solution {
-    public static int func(int i,int j,String str1,String str2,int dp[][])
+    public static int func(int ind,ArrayList<Integer> rack,int prev,int dp[][])
     {
-       if(i==0)
-        return j;
-        if(j==0)
-            return i;
-            if(dp[i][j]!=-1)
-                return dp[i][j];
-        if(str1.charAt(i-1)==str2.charAt(j-1))
-            return dp[i][j]=func(i-1,j-1,str1,str2,dp);
-        else 
-        return dp[i][j]=1+Math.min(func(i-1,j,str1,str2,dp),Math.min(func(i,j-1,str1,str2,dp),func(i-1,j-1,str1,str2,dp)));
-    }
-    public static int editDistance(String str1, String str2) {
-        //Your code goes here
-        int dp[][]=new int[str1.length()+1][str2.length()+1];
-        // for(int i[]:dp)
-        // {
-        //     Arrays.fill(i,-1);
-        // }
-        for(int i=0;i<=str2.length();i++)
-            dp[0][i]=i;
-        for(int i=0;i<=str1.length();i++)
-            dp[i][0]=i;
-        for(int i=1;i<=str1.length();i++)
+        if(ind==0)
         {
-            for(int j=1;j<=str2.length();j++)
-            {
-                 if(str1.charAt(i-1)==str2.charAt(j-1))
-                    dp[i][j]=dp[i-1][j-1];
-                else 
-                    dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
-
-            }
+             if(prev==-1)
+                return rack.get(ind);
+            return (rack.get(ind)<rack.get(prev))?rack.get(ind):0;
         }
-        return dp[str1.length()][str2.length()];
+        if(dp[ind][prev+1]!=-1)
+            return dp[ind][prev+1];
+        int pick =0;
+
+        if( prev==-1 || rack.get(ind)<rack.get(prev))
+         pick = rack.get(ind)+func(ind-1,rack,ind,dp);
+        int notpick=func(ind-1,rack,prev,dp);
+        return dp[ind][prev+1]=Math.max(pick,notpick);
     }
+    public static int maxIncreasingDumbbellsSum(ArrayList<Integer> rack, int n) {
+        // Write your code here.
+        int dp[][]=new int[n][n+1];
+        for(int i[]:dp)
+            Arrays.fill(i,-1);
+
+        
+        return func(n-1,rack,-1,dp);
+    }
+
 }
-
-
-

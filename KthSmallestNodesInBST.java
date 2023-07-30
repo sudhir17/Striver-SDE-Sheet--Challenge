417a3e0a493ef433d99b56a28db3cc924a37cab0
sudhir17 <65382006+sudhir17@users.noreply.github.com>
2023-07-30 10:10:30 +0530

    Update KthSmallestNodesInBST.java

--- KthSmallestNodesInBST.java
+++ KthSmallestNodesInBST.java
@@ -1,26 +1,29 @@
 import java.util.* ;
 import java.io.*; 
-/*************************************************************
-    Following is the Binary Search Tree node structure
+/************************************************************
 
-	class TreeNode<T> {
+    Following is the TreeNode class structure
+
+	class TreeNode<T> 
+	{
 	    public T data;
 	    public TreeNode<T> left;
 	    public TreeNode<T> right;
 
-	    TreeNode(T data) {
+	    TreeNode(T data) 
+	    {
 	        this.data = data;
 	        left = null;
 	        right = null;
 	    }
 	}
 
-*************************************************************/
+************************************************************/
 
-import java.util.ArrayList;
 
-public class Solution {
-	public static void func(TreeNode<Integer> root, PriorityQueue<Integer> pq,int k)
+public class Solution 
+{
+		public static void func(TreeNode<Integer> root, PriorityQueue<Integer> pq,int k)
 	{
 		if(root==null)
 			return;
@@ -30,12 +33,15 @@ public class Solution {
 			pq.poll();
 		func(root.right,pq,k);
 	}
-	public static int KthLargestNumber(TreeNode<Integer> root, int k) {
-		// Write your code here.
-		PriorityQueue<Integer> pq=new PriorityQueue<>();
+
+	public static int kthSmallest(TreeNode<Integer> root, int k) 
+	{
+		PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
 		func(root,pq,k);
 		if(pq.size()<k)
 			return -1;
 		return pq.poll();
+		//  Write your code here.
 	}
+
 }

1e508f7663f02689b4458674ff672328d006f136
sudhir17 <65382006+sudhir17@users.noreply.github.com>
2023-07-30 09:53:23 +0530

    Create PostOrderTraversal.java

---
+++ PostOrderTraversal.java
@@ -0,0 +1,37 @@
+import java.util.* ;
+import java.io.*; 
+/*
+    
+    Following is the Binary Tree node structure:
+
+    public class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.data = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+           this.data = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+*/
+
+public class Solution {
+    public static void func(TreeNode root,List<Integer> list)
+    {
+        if(root==null)
+            return;
+        func(root.left,list);
+        func(root.right,list);
+        list.add(root.data);
+    }
+    public static List < Integer > getPostOrderTraversal(TreeNode root) {
+        List<Integer> list=new ArrayList<>();
+        func(root,list);
+        return list;
+    	// Write your code here.
+    }
+}
